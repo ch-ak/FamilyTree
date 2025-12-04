29ed8cb96d7944aefeecef1b03dc8f0a804cc79e
ch-ak <61340762+ch-ak@users.noreply.github.com>
2025-12-04 09:44:16 -0500

    Initial Commit

---
+++ FamilyTree/FamilyTreeApp.swift
@@ -0,0 +1,17 @@
+//
+//  FamilyTreeApp.swift
+//  FamilyTree
+//
+//  Created by Chakri Kotcherlakota on 12/4/25.
+//
+
+import SwiftUI
+
+@main
+struct FamilyTreeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FamilyTreeTests/FamilyTreeTests.swift
@@ -0,0 +1,17 @@
+//
+//  FamilyTreeTests.swift
+//  FamilyTreeTests
+//
+//  Created by Chakri Kotcherlakota on 12/4/25.
+//
+
+import Testing
+@testable import FamilyTree
+
+struct FamilyTreeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
